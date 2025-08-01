22af2cf752bcb8a483ba3ea0f5e4d5f9eed55989
James Acero <jacero43464@liceo.edu.ph>
2025-08-01 11:17:07 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(string[] args) {
-        System.out.println("Main Branch!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int totalNumber = 0;
+
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter number " + i + ": ");
+            int number = scanner.nextInt();
+            totalNumber += number;
+        }
+        
+        System.out.println("Total Number: " + totalNumber);
     }
 }
