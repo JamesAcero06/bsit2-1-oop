66882e687b1b0ffb5891e73cacc2cc1cfe51de25
James Acero <jacero43464@liceo.edu.ph>
2025-08-04 17:10:03 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,58 @@
-public class Main {
-    public static void main(string[] args) {
-        System.out.println("Main Branch!");
+public class Book {
+
+    String title;
+    String author;
+    int pages;
+    boolean isAvailable;
+
+
+    public Book(String title, String author, int pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isAvailable = true;
+        System.out.println("A new book '" + title + "' by " + author + " has been added to the library!");
+    }
+
+
+    public void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Pages: " + pages);
+        System.out.println("Available: " + (isAvailable ? "Yes" : "No"));
+        System.out.println();
+    }
+
+
+    public void borrowBook() {
+        if (isAvailable) {
+            isAvailable = false;
+            System.out.println("'" + title + "' has been borrowed.");
+        } else {
+            System.out.println("'" + title + "' is currently not available.");
+        }
+    }
+
+
+    public void returnBook() {
+        if (!isAvailable) {
+            isAvailable = true;
+            System.out.println("'" + title + "' has been returned.");
+        } else {
+            System.out.println("'" + title + "' was not borrowed.");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Book book1 = new Book("Juan Tamad", "James Acero", 300);
+        Book book2 = new Book("Jame s Tamad", "Ja", 280);
+        Book book3 = new Book("Tamad Juan", "Juan", 180);
+
+        book1.displayInfo();
+        book2.borrowBook();
+        book2.displayInfo();
+        book2.returnBook();
+        book2.displayInfo();
     }
 }
