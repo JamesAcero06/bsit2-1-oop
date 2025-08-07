f0e584e6b34a1c3063f07bec62744ee9e9ea0509
James Acero <jacero43464@liceo.edu.ph>
2025-08-07 10:40:32 +0800

    hello

---
+++ bsit2-1-oop/src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String Student = "Student Information";
+        String  firstname = "Alice";
+        int Age = 20;
+        String Course = "BSIT";
+        double grade1 = 85.0;
+        double grade2 = 90.0;
+        double grade3 = 88.0;
+        double average = 87.67;
+        String  grade = "B";
+        String status = "PASSING";
+
+
+        System.out.println("Student: " + Student + ":");
+        System.out.println("name: " + firstname + " " + "Age:" + "19 " +"Course:" + Course);
+        System.out.println("double: " + grade1 + "," + grade2 + "," + grade3);
+        System.out.println("average: " + average);
+        System.out.println("grade: " + grade);
+        System.out.println("status: " + status);
+
+
+        String  Firstname = "Bob";
+        int age = 19;
+        String course = "BSCS";
+        double Grade1 = 92.0;
+        double Grade2 = 95.0;
+        double Grade3 = 89.0;
+        double Average = 92.00;
+        String  Grade = "A";
+        String Status = "PASSING";
+
+
+        System.out.println("Student: " + Student + ":");
+        System.out.println("name: " + firstname + " " + "Age:" + "20 " +"Course:" + Course);
+        System.out.println("double: " + grade1 + "," + grade2 + "," + grade3);
+        System.out.println("average: " + average);
+        System.out.println("grade: " + grade);
+        System.out.println("status: " + status);
+
+
+
+
+
+
+    }
+    }
