454142dc5dac36e5b4bc429ddc26b0eb9cc81c1c
James Acero <jacero43464@liceo.edu.ph>
2025-10-02 11:39:22 +0800

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,65 +1,19 @@
-public class Student extends Abstract User {
-    private String name;
+
+public class Student extends User {
     private String studentId;
+    private String major;
 
-    public Student(String name, String studentId) {
-        this.name = name;
+    public Student(String studentId, String userId, String name, String email, String major) {
+        super(userId, name, email);
         this.studentId = studentId;
+        this.major = major;
     }
 
-    @Override
-    public String getUserType() {
-        return "Student";
-    }
+    public String getStudentId() { return studentId; }
+    public String getMajor() { return major; }
 
     @Override
     public int getMaxBorrowLimit() {
-        return 3;
+        return 5;
     }
-
-    @Override
-    public void borrowMedia(Media media) {
-        if (media.isAvailable()) {
-            media.borrowItem();
-            System.out.println(name + " (" + getUserType() + ") borrowed: " + media.getTitle());
-        } else {
-            System.out.println("Sorry, " + media.getTitle() + " is not available");
-        }
-    }
-
-    @Override
-    public void returnMedia(Media media) {
-        media.returnItem();
-        System.out.println(name + " (" + getUserType() + ") returned: " + media.getTitle());
-    }
-}
-
-this.title = title;
-this.author = author;
-this.isCheckedOut = false;
-        this.borrowerName = null;
-        }
-
-public String getItemInfo() {
-    return String.format("%s: %s by %s (%s)", getItemType(), title, author, isCheckedOut ? "Checked out by " + borrowerName : "Available");
-}
-
-public void checkOut(String borrowerName) {
-    if (!isCheckedOut) {
-        this.isCheckedOut = true;
-        this.borrowerName = borrowerName;
-    }
-}
-
-public void checkIn() {
-    this.isCheckedOut = false;
-    this.borrowerName = null;
-}
-
-public String getItemId() { return itemId; }
-public String getTitle() { return title; }
-public String getAuthor() { return author; }
-
-public abstract String getItemType();
-public abstract double calculateLateFee(int daysLate);
 }
