74066a2bf7cfd6a0089fc8ffb265324e6624ee24
James Acero <jacero43464@liceo.edu.ph>
2025-10-02 10:54:47 +0800

    Update Borrowed Interface.java

--- src/Borrowed Interface.java
+++ src/Borrowed Interface.java
@@ -1,1 +1,29 @@
+public class borrowed Interface {
+this.title = title;
+this.author = author;
+this.isCheckedOut = false;
+this.borrowerName = null;
+}
 
+public String getItemInfo() {
+    return String.format("%s: %s by %s (%s)", getItemType(), title, author, isCheckedOut ? "Checked out by " + borrowerName : "Available");
+}
+
+public void checkOut(String borrowerName) {
+    if (!isCheckedOut) {
+        this.isCheckedOut = true;
+        this.borrowerName = borrowerName;
+    }
+}
+
+public void checkIn() {
+    this.isCheckedOut = false;
+    this.borrowerName = null;
+}
+
+public String getItemId() { return itemId; }
+public String getTitle() { return title; }
+public String getAuthor() { return author; }
+
+public abstract String getItemType(); 
+public abstract double calculateLateFee(int daysLate);
